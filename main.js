cd2c859d7c59df5cfc81a6452feca13bb4022735
Alexander L. Wolf <109392016+AlexanderLucasWolf@users.noreply.github.com>
2022-09-30 13:57:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            stockItemOne: 9,
+            onSaleItemOne: true,
+            priceItemOne: 99.99,
+
+            stockItemTwo: 312,
+            onSaleItemTwo: true,
+            priceItemTwo: 149.99,
+
+            stockItemThree: 146,
+            onSaleItemThree: false,
+            priceItemThree: 299.99,
+
+            stockItemFour: 18,
+            onSaleItemFour: false,
+            priceItemFour: 159.99,
+
+            stockItemFive: 3,
+            onSaleItemFive: false,
+            priceItemFive: 99.99,
+
+            stockItemSix: 89,
+            onSaleItemSix: false,
+            priceItemSix: 249.99,
+
+            stockItemSeven: 0,
+            onSaleItemSeven: false,
+            priceItemSeven: 199.99,
+
+            stockItemEight: 314,
+            onSaleItemEight: false,
+            priceItemEight: 299.99,
+
+            cart: 0
+        }
+    }
+})
